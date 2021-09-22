08616006c7c4eb238b8ef50c95d1cc9e851f1fab
faridAbinah <faridabinah@hotmail.com>
2021-09-22 17:00:21 -0400

    Refactored code to seperate input functions into seperate input file

---
+++ input.js
@@ -0,0 +1,33 @@
+//setup interface to handle user input from stdin
+/**
+ * 
+ * In Client Setup, we returned a conn object from the connect function that allowed us to interact with the server.
+
+Similarly, the stdin object returned by setupInput will allow us to listen for keyboard input and react to it.
+ */
+const handleUserInput = function (input) {
+  // your code here
+  if(input === '\u0003') {
+    process.exit();
+  }
+
+  // if(input === 'l') {
+  //   process.exit();
+  // }
+
+  console.log(input);
+};
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+//Here is an event handler listening for any data we type to the stdin,  then it will call our callback function handle user input
+//Note that WE ARE NOT CALLING HANDLERUserInput, we are simply passing this function as a parameter. This is why in our function expression above we still have access to the input parameter
+  stdin.on('data',handleUserInput);
+
+  return stdin;
+};
+
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,39 +1,11 @@
 
 //const connect = require('./client');
 const {connect, net} = require('./client');
-//this is how we unpack data and functions we are recieving from our clie
+//this is how we unpack data and functions we are recieving from our client
+const {setupInput} = require('./input');
+//We dont have to unpack our handleUserInput function because it is only called by our setupInput function and it is already in the same file as setInput
 
-// setup interface to handle user input from stdin
-/**
- * 
- * In Client Setup, we returned a conn object from the connect function that allowed us to interact with the server.
 
-Similarly, the stdin object returned by setupInput will allow us to listen for keyboard input and react to it.
- */
-const handleUserInput = function (input) {
-  // your code here
-  if(input === '\u0003') {
-    process.exit();
-  }
-
-  // if(input === 'l') {
-  //   process.exit();
-  // }
-
-  console.log(input);
-};
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-//Here is an event handler listening for any data we type to the stdin,  then it will call our callback function handle user input
-//Note that WE ARE NOT CALLING HANDLERUserInput, we are simply passing this function as a parameter. This is why in our function expression above we still have access to the input parameter
-  stdin.on('data',handleUserInput);
-
-  return stdin;
-};
 
 
 
