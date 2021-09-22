f0bcc3da9e9e904d4527fa0e8e80e6639fef6095
faridAbinah <faridabinah@hotmail.com>
2021-09-22 16:46:16 -0400

    created event handler to listen for data on stdin, also implemented handle user input function to exit if ctrl + c is pressed

--- play.js
+++ play.js
@@ -3,5 +3,43 @@
 const {connect, net} = require('./client');
 //this is how we unpack data and functions we are recieving from our clie
 
+// setup interface to handle user input from stdin
+/**
+ * 
+ * In Client Setup, we returned a conn object from the connect function that allowed us to interact with the server.
+
+Similarly, the stdin object returned by setupInput will allow us to listen for keyboard input and react to it.
+ */
+const handleUserInput = function (input) {
+  // your code here
+  if(input === '\u0003') {
+    process.exit();
+  }
+
+  // if(input === 'l') {
+  //   process.exit();
+  // }
+
+  console.log(input);
+};
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+//Here is an event handler listening for any data we type to the stdin,  then it will call our callback function handle user input
+//Note that WE ARE NOT CALLING HANDLERUserInput, we are simply passing this function as a parameter. This is why in our function expression above we still have access to the input parameter
+  stdin.on('data',handleUserInput);
+
+  return stdin;
+};
+
+
+
+
+
 console.log("Connecting ...");
 connect();
+
+setupInput();
