f6607b592423721d263c744265c1f5b205618f78
faridAbinah <faridabinah@hotmail.com>
2021-09-22 14:46:47 -0400

    Created a connection to a snake game

---
+++ play.js
@@ -0,0 +1,34 @@
+const { Console } = require("console");
+const net = require("net");
+
+// establishes a connection with the game server
+//used createConnection function to create an object called conn
+//conn object represents the connection that you have with the server
+//Conn object is filled with useful properties that can now be used to interact with the server.
+const connect = function () {
+  const conn = net.createConnection({
+    host:'135.23.223.133', // IP address here,
+    port: 50542 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  
+  conn.on("connect", () => {
+    console.log("I have connected");
+  });
+
+  conn.on("data", (data) => {
+    console.log("DATA CAME IN");
+    console.log(data);
+  })
+
+  return conn;
+};
+
+
+
+
+console.log("Connecting ...");
+connect();
